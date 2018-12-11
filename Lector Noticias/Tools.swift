5a86f0beebf8a79018ad1510c32915078bc6dcf0
Dev1 <Dev1@GET12-07.local>
2018-12-11 13:03:50 +0100

    Intento recibir por terminal datos 01

--- Lector Noticias/Tools.swift
+++ Lector Noticias/Tools.swift
@@ -9,4 +9,43 @@
 import Foundation
 import CommonCrypto
 
-var baseURL = URL(string: "https://applecoding.com/wp-json/wp/v2")!
+var inicioURL = URL(string: "https://applecoding.com/wp-json/wp/v2")!
+
+func conectarRSS() {
+   let queryPost = URLQueryItem (name: "p", value: "posts")
+   let queryCategorias = URLQueryItem (name: "categoria", value: "categories")
+   let queryAutores = URLQueryItem (name: "author", value: "users")
+
+   var url = URLComponents()
+   url.scheme = inicioURL.scheme
+   url.host = inicioURL.host
+   url.path = inicioURL.path
+   url.queryItems = [queryPost, queryCategorias, queryAutores]
+   
+   let session = URLSession.shared
+   var request = URLRequest(url: url.url!.appendingPathComponent("posts"))
+   request.httpMethod = "GET"
+   request.addValue("*/*", forHTTPHeaderField: "Accept")
+   session.dataTask(with: request) { (data, response, error) in
+      guard let data = data, let response = response as? HTTPURLResponse, error == nil else {
+         if let error = error {
+            print("Error en la comunicación \(error)")
+         }
+         return
+      }
+      if response.statusCode == 200 {
+         print(String(data: data, encoding: .utf8)!)
+         //cargar(datos: data)
+      } else {
+         print(response.statusCode)
+      }
+      }.resume()
+}
+
+// OBTENER FECHA DE PUBLICACIÓN
+//func getDateTime() -> String {
+//   let fecha = Date()
+//   let formatter = DateFormatter()
+//   formatter.dateFormat = "ddMMyyyyhhmmss"
+//   return formatter.string(from: fecha)
+//}
