44ec0152c6988bbb50b3678ec4442b2786400ec5
Dev1 <Dev1@GET12-07.local>
2018-12-11 13:47:25 +0100

    final de clase COMMIT-PUSH

---
+++ Lector Noticias/Modelo.swift
@@ -0,0 +1,15 @@
+//
+//  Modelo.swift
+//  Lector Noticias
+//
+//  Created by Dev1 on 11/12/2018.
+//  Copyright © 2018 Dev1. All rights reserved.
+//
+
+import Foundation
+
+struct ROOT_JSON: Codable {
+   struct Data: Codable {
+      //Pendiente desarrollo para recibir los datos JSON.
+   }
+}
