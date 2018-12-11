30802889561e30b0b7a9b0a27b608004ac9f29fa
Dev1 <Dev1@GET12-07.local>
2018-12-11 09:29:30 +0100

    Initial Commit

---
+++ Lector Noticias/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lector Noticias
+//
+//  Created by Dev1 on 11/12/2018.
+//  Copyright © 2018 Dev1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+
+}
+
