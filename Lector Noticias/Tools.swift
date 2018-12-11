f8c70f88e5d3c322af7b8c5edffe337487a6868a
Dev1 <Dev1@GET12-07.local>
2018-12-11 10:12:06 +0100

    prueba funcional Git

---
+++ Lector Noticias/Tools.swift
@@ -0,0 +1,12 @@
+//
+//  Tools.swift
+//  Lector Noticias
+//
+//  Created by Dev1 on 11/12/2018.
+//  Copyright © 2018 Dev1. All rights reserved.
+//
+
+import Foundation
+import CommonCrypto
+
+var baseURL = URL(string: "https://applecoding.com/wp-json/wp/v2")!
